9e9ac01531afe479636a68a2d31001502a3a2fb4
Natsudraganeel <149077586+Natsudraganeel@users.noreply.github.com>
2024-12-31 21:21:37 +0530

    Update Login.jsx

--- client/src/Login.jsx
+++ client/src/Login.jsx
@@ -57,6 +57,7 @@ console.log(err.message)
          if(res.data.success===true){
           console.log(res.data.token);
           dispatch(settoken(res.data.token));
+           document.cookie="token="+res.data.token;
            localStorage.setItem("token", res.data.token);
               nav("/");
          }
