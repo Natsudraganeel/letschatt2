00c4606009788c4f9583b3f080b8b2834124a479
Natsudraganeel <149077586+Natsudraganeel@users.noreply.github.com>
2024-12-31 12:34:21 +0530

    Update Authroute.js

--- server/Routers/Authroute.js
+++ server/Routers/Authroute.js
@@ -4,7 +4,7 @@ import {registercontroller,logincontroller,userdetails,googleauth,updateinfocont
 const router=express.Router();
 router.post("/register",registercontroller);
 router.post("/login",logincontroller);
-router.get("/user-details",userdetails)
+router.post("/user-details",userdetails)
 router.get("/google",googleauth);
 router.put("/updateinfo",updateinfocontroller);
 router.get("/search/:id/:keyword",searchcontroller);
