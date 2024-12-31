fa2b0f2c8f175b772014163ce6e5ed08c06f9f4e
Natsudraganeel <149077586+Natsudraganeel@users.noreply.github.com>
2024-12-31 11:11:54 +0530

    Update forgotpassword.jsx

--- client/src/forgotpassword.jsx
+++ client/src/forgotpassword.jsx
@@ -22,7 +22,7 @@ export default function Forgotpassword(){
             event.preventDefault();
     console.log(email);
             try{
-              const res=await axios.post("http://localhost:3000/api/auth/forgotpassword",{
+              const res=await axios.post("https://letschatt2-backend.onrender.com/api/auth/forgotpassword",{
                 email
               })
               console.log(res.data);
@@ -106,7 +106,7 @@ export default function Forgotpassword(){
             try{
             const res=await axios({
                 method:"post",
-                url:"http://localhost:3000/api/auth/forgotpassword",
+                url:"https://letschatt2-backend.onrender.com/api/auth/forgotpassword",
                 data:{
                   email,
                   
