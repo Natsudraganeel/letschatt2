94b7f8045eb767faaf372e8d8b1bc55668423504
Natsudraganeel <149077586+Natsudraganeel@users.noreply.github.com>
2025-07-03 20:38:41 +0530

    Update authcontroller.js

--- server/Controllers/authcontroller.js
+++ server/Controllers/authcontroller.js
@@ -126,7 +126,7 @@ else{
        let token= JWT.sign({id:user._id,email:user.email},process.env.JWT_SECRET,{expiresIn:'3h'});//signing the payload which contains _id of the user
        // didnot do await JWT as await has no effect on it
        
-     res.cookie('token',token,{maxAge: 1000 * 60 * 180}).send({// .cookie i found in gfg cookie-parser article
+     res.cookie('token',token,{maxAge: 1000 * 60 * 180,secure:true,sameSite:'None', httpOnly: false}).send({// .cookie i found in gfg cookie-parser article
             success:true,
             message: 'login successful',
          token:token});}
@@ -281,7 +281,7 @@ export const forgotcontroller=async(req,res)=>{
     else{
       //create token for authentication
       const token= JWT.sign({id:user._id,email:user.email},process.env.JWT_SECRET,{expiresIn:'3h'});
-     res.cookie('token',token,{maxAge: 1000 * 60 * 180}).send({// .cookie i found in gfg cookie-parser article
+     res.cookie('token',token,{maxAge: 1000 * 60 * 180,secure:true,sameSite:'None', httpOnly: false}).send({// .cookie i found in gfg cookie-parser article
             success:true,
             message: 'login successful',
          token:token});}
