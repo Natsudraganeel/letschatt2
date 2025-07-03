02282eed51249fa0a1bf52035d172b663f150413
Natsudraganeel <149077586+Natsudraganeel@users.noreply.github.com>
2025-07-03 19:34:29 +0530

    Update Getuserdetails.js

--- server/Helpers/Getuserdetails.js
+++ server/Helpers/Getuserdetails.js
@@ -8,8 +8,17 @@ export const getuserdetails=async(token)=>{
     }
  }
  else{
+    try{
     const decode=jwt.verify(token,process.env.JWT_SECRET);
+    console.log("the decode",decode);
     const user=await User.findById(decode.id).select("-password");
     return user;
+    }
+    catch(err){
+            if(err.name==="TokenExpiredError"){
+        return {success:false,message:"Session Expired.Relogin!"};
+      }
+      return {success:false,message:err.message};
+    }
  }
 }
